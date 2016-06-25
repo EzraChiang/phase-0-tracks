3bf746c633a82ba5354272e08a43231f1cecd3b5
Ezra J. Chiang <ezra.j.chiang@gmail.com>
2016-06-25 15:42:04 -0700

    write examples of .each and .map! with arrays and hashes

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,3 +4,30 @@ def Sun_Moon
 end 
 
 Sun_Moon { |dog1, dog2| puts "#{dog1} and #{dog2} you were the sun and moon to me!"}
+
+
+
+
+Festivals = ['Escape', 'Led', 'Beyond', 'Coachella', 'Edc']
+
+Friends = {'Carina' => 21, 'Aimie' => 20, 'Jake' => 21, 'Ezra' => 23} 
+
+
+puts "Festival Data"
+p Festivals 
+
+Festivals.each do |event|
+	puts "You went to #{event}!"
+end
+
+Festivals.map! do |event|
+	puts "You went to #{event}!"
+	event.upcase
+end
+
+puts "After.map call:"
+p Festivals 
+
+Friends.each do |name, age| 
+	puts "Your friend is #{name} and they are #{age}"
+end
