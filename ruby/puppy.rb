ee35a263ac445b6051561830b6c3ada1ca8050a1
Ezra J. Chiang <ezra.j.chiang@gmail.com>
2016-06-30 01:23:27 -0700

    do release 0, design a class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin 
+Species-------
+Puppy
+
+Characteristics-----
+Fur length: varies
+Friendly: Yes
+Eye count: 2
+Name: varies
+Color: varies
+
+Behavior------
+Jump
+Wag tail
+Bark
+Lick
+
+
+
+=end 
