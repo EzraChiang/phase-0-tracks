4dc3d80d7b73b940fcdbc2bfacaa95db6e82975c
Ezra J. Chiang <ezra.j.chiang@gmail.com>
2016-07-01 21:28:30 -0700

    complete release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,28 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(qty)
+  	qty.times {puts "Woof!"}
+  end
+  
+  def roll_over
+  	puts "*rolls over*"
+  end
+  
+  def dog_years(age)
+  	p age/7
+  end
+  
+  def shake_hands
+  	puts "*shake hands*"
+  end
 
 end
 
+#Carlos = Puppy.new 
+#Carlos.fetch("ball")
+#Carlos.speak(5)
+#Carlos.roll_over 
+#Carlos.dog_years(21)
+#Carlos.shake_hands
