bcfd90640518ca7d72570d33036fa98d572957e4
Ezra J. Chiang <ezra.j.chiang@gmail.com>
2016-07-01 22:51:13 -0700

    complete release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -6,30 +6,67 @@ class Puppy
   end
   
   def speak(qty)
-  	qty.times {puts "Woof!"}
+    qty.times {puts "Woof!"}
   end
   
   def roll_over
-  	puts "*rolls over*"
+    puts "*rolls over*"
   end
   
   def dog_years(age)
-  	p age/7
+    p age/7
   end
   
   def shake_hands
-  	puts "*shake hands*"
+    puts "*shake hands*"
   end
   
-  def initialize 
-  	puts "Initializing new puppy instance..."
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
   end
 
 end
 
-#Carlos = Puppy.new 
-#Carlos.fetch("ball")
-#Carlos.speak(5)
-#Carlos.roll_over 
-#Carlos.dog_years(21)
-#Carlos.shake_hands
+Carlos = Puppy.new('Carlos') 
+Carlos.fetch("ball")
+Carlos.speak(5)
+Carlos.roll_over 
+Carlos.dog_years(21)
+Carlos.shake_hands
+
+class Heroes 
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def fly
+    puts 'Fly so high'
+  end
+  
+  def heat_vision
+    puts '*Heat_Vision*'
+  end
+  
+end
+
+Hero = []
+50.times do
+  Hero << Heroes.new('Superman')
+end 
+
+p Hero 
+
+Hero.each do |heroes|
+  puts heroes.name
+  puts heroes.fly 
+  puts heroes.heat_vision 
+end
