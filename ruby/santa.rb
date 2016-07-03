3e673ce1e957e2b644171723838d1b1ea6ca0d66
Ezra J. Chiang <ezra.j.chiang@gmail.com>
2016-07-02 21:30:30 -0700

    complete release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,7 @@
 class Santa 
 	
-	attr_reader :age, :ethnicity, :reindeer_ranking 
-	attr_accessor :gender 
+	attr_reader :ethnicity, :reindeer_ranking 
+	attr_accessor :gender, :age  
 
 	def speak 
 	puts "Ho, ho, ho! Haaaappy holidays!"
@@ -16,7 +16,7 @@ class Santa
 	@gender = gender
 	@ethnicity = ethnicity 
 	@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	@age = 0 
+	@age = rand(140)
 	
 	puts "Initializing Santa instance..."
 	end
@@ -39,18 +39,30 @@ class Santa
 end 
 
 
-santas = []
-example_genders = ["alien", "fossil", "reptilian", "male", "female", "animal", "robot"]
-example_ethnicities = ["Blasian", "Whitino", "Singamexican", "Japanese-African", "Egyption-God", "Atlantian", "Perutugal"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+#santas = []
+#example_genders = ["alien", "fossil", "reptilian", "male", "female", "animal", "robot"]
+#example_ethnicities = ["Blasian", "Whitino", "Singamexican", "Japanese-African", "Egyption-God", "Atlantian", "Perutugal"]
+#example_genders.length.times do |i|
+  #santas << Santa.new(example_genders[i], example_ethnicities[i])
+#end
 
-santa = Santa.new('male', 'Blasian')
-santa.gender = 'female'
-p santa 
-santa.celebrate_birthday
-santa.celebrate_birthday
-santa.get_mad_at('Rudolph')
-santa.get_mad_at('Vixen')
-santa.get_mad_at('Vixen')
+#santa = Santa.new('male', 'Blasian')
+#santa.gender = 'female'
+#p santa 
+#santa.celebrate_birthday
+#santa.celebrate_birthday
+#santa.get_mad_at('Rudolph')
+#santa.get_mad_at('Vixen')
+#santa.get_mad_at('Vixen')
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+reindeers = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+cookies = ["Vanila", "Chocolate Chip", "Fudge", "Caramel"] 
+100.times do 
+p santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+p santa.age
+p santa.speak 
+p santa.eat_milk_and_cookies(cookies.sample)
+p santa.get_mad_at(reindeers.sample)
+p santa.celebrate_birthday
+end 
