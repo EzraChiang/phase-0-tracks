baa68d67be37199edb4ec5212afb26c05a23fb48
Ezra J. Chiang <ezra.j.chiang@gmail.com>
2016-07-07 15:34:58 -0700

    complete release 0

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -12,3 +12,46 @@
 #2. Pass 
 #3. Steal 
 #4. Block 
+
+class Basketball_Player 
+
+attr_reader :country
+attr_accessor :name, :age, :university, :position 
+
+def initialize(name, age, university, position)
+@name = name
+@age = age 
+@university = university  
+@position = position
+@country = "USA"
+end 
+
+def shoot(type)
+case type
+when "dunk"
+	puts "*Slam Dunk*! 2 points!"
+when "lay up"
+	puts "*Lay up*! 2 points!"
+when "three pointer"
+	puts "*Threeeeee!!! 3 points!" 
+when "jumper"
+	puts "*Nothing but net*! 2 points!"
+else
+	puts "BRICK!"
+end
+end 
+
+def pass  
+	puts "Assist! nice pass #{name}"
+end
+
+def steal
+	puts "Highway robbery! Nice steal #{name}!"
+end
+
+def block 
+	puts "Not in #{name}'s house. No no no. BLOCK!"
+end
+
+
+end 
