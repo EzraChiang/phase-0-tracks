fcf1f929a831c2d42f5aa4b3394908f20062ebca
Ezra J. Chiang <ezra.j.chiang@gmail.com>
2016-07-07 13:39:32 -0700

    complete release 0

---
+++ ruby/solo.rb
@@ -0,0 +1,14 @@
+#Release 0
+#Basketball_Player class 
+#-----Attributes------
+#1. Name (String)
+#2. Age (Integer)
+#3. University (String)
+#4. Position (i.e. PG, SG, SF, PF, C) (String)
+
+
+#----Methods------
+#1. Shoot(take an argument of what type of shot)
+#2. Pass 
+#3. Steal 
+#4. Block 
