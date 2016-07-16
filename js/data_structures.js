ea52f64dbe1a4883429209dd29ac179fe6a38e5a
Ezra J. Chiang <ezra.j.chiang@gmail.com>
2016-07-16 14:57:26 -0700

    complete release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,13 @@ var horses = ['Tom', 'Henry', 'Charlene', 'Sarah'];
 colors.push('yellow');
 horses.push('Roger');
 
+//console.log(colors);
+//console.log(horses);
+
+var assignment = {};
+
+for (var i = 0; i < horses.length; i++) {
+	assignment[horses[i]] = colors[i];
+}
+
+console.log(assignment);
