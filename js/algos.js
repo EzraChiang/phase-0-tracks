3b3a3bd4238299d2e50721bfb6d36ba115e81415
Ezra J. Chiang <ezra.j.chiang@gmail.com>
2016-07-17 01:28:47 -0700

    complete release 2

--- js/algos.js
+++ js/algos.js
@@ -45,8 +45,58 @@ function finder(obj1, obj2) {
 
 //var test1 = {name: "Steven", age: 54};
 //var test2 = {name: "Tamir", age: 54};
-var test3 = {name: "Steven", age: 54};
-var test4 = {name: "Tamir", age: 55};
+//var test3 = {name: "Steven", age: 54};
+//var test4 = {name: "Tamir", age: 55};
 
 //console.log(finder(test1, test2));
-console.log(finder(test3, test4));
+//console.log(finder(test3, test4));
+
+
+//Release 2
+//Write a function that takes an integer for length, and builds and returns an array of strings of the given length. 
+//So if we ran your function with an argument of 3, we would get an array of 3 random words back (the words don't have to be actual sensical English words -- "nnnnfph" totally counts)
+//The words should be of randomly varying length, with a minimum of 1 letter and a maximum of 10 letters.
+//Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result.
+//function takes an integer
+//builds an array of strings with the given integer
+//each string in the array has a different length (min: 1, max: 10)
+//create an array variable and set it equal to an empty array
+//create an alphabet variable with each letter in the alphabet
+//write a for loop and set the counter < the integer that was taken as an argument. 
+//set variables for random number(min:1, max: 10) and one for the generated word set to a string.
+//write another for loop that sets the counter less than the random number.
+//write a variable that randomizes a number between 1 to 26. 
+//set the word variable += the alphabet variable indexed with our randomized number between 1 to 26 and this will run for however long our random number between (1 to 10 was).
+//close this loop then push our word into the array.
+//loop through until it reaches the argument set initially.
+
+function randomarray(integer) {
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	var array = [];
+	
+	for (var i = 0; i < integer; i++) {
+		var randnumber = Math.floor(Math.random() * 10) + 1;
+		var word = '';
+		
+		for (var counter = 0; counter < randnumber; counter++) {
+		var alpharand = Math.floor(Math.random() * 26);
+		word += alphabet[alpharand];
+		}
+		array.push(word);
+	}
+	return array;
+}
+
+for (var driver = 0; driver < 10; driver++) {
+	integer = Math.floor(Math.random() * 5) + 1;
+	var drivertest = randomarray(integer);
+	console.log("--------");
+	console.log("Array:");
+	console.log(drivertest);
+	console.log("--------");
+	var drivertest2 = longest(drivertest);
+	console.log("Longest Word:")
+	console.log(drivertest2);
+	}
+
+
