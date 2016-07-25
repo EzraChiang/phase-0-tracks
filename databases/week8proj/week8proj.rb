1e3feb67b73daf0bac484b74979bc57becb0f8b2
Ezra J. Chiang <ezra.j.chiang@gmail.com>
2016-07-25 09:25:12 -0700

    add user interface

--- databases/week8proj/week8proj.rb
+++ databases/week8proj/week8proj.rb
@@ -16,5 +16,29 @@ SQL
 ws.execute(create_table_spending)
 
 def create_spending(ws, item, quanity, price, refrigerated)
-	db.execute("INSERT INTO weekly_spending (item, quantity, price, refrigerated) VALUES (?, ?, ?, ?)", [item, quantity, price, refrigerated])
+	ws.execute("INSERT INTO weekly_spending (item, quantity, price, refrigerated) VALUES (?, ?, ?, ?)", [item, quantity, price, refrigerated])
 end
+
+Loop do 
+
+puts "What do you need to buy?"
+item = gets.chomp
+
+puts "How many do you need to buy?"
+quantity = gets.chomp.to_i 
+
+puts "How much does each cost?"
+price = gets.chomp.to_i
+
+puts "Does this need to be refrigerated? (Y/N)?"
+answer = gets.chomp
+refrigerated = answer == "Y"
+
+create_spending(ws, item, quantity, price, refrigerated)
+
+puts "Add another item? Type 'Done' to exit"
+add = gets.chomp 
+break if add == 'Done'
+
+
+
